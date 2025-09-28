ecf975466c60ff361b08e23889730b9e922fce93
lechalepecha <aleksejserakov45@gmail.com>
2025-09-28 14:34:36 +0700

    final the modifier

--- app/src/main/java/com/example/modifiersdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifiersdemo/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.modifiersdemo
 
-import android.R
+
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,11 +16,17 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.Alignment
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.modifiersdemo.ui.theme.ModifiersDemoTheme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,14 +46,30 @@ class MainActivity : ComponentActivity() {
 fun DemoScreen(modifier: Modifier = Modifier){
     val mymodifier = modifier
         .border(width = 2.dp, color = Color.Gray)
-        .padding(all = 35.dp)
+        .padding(all = 15.dp)
 
-    Text(
-        "Hello Compose",
-        mymodifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Bold
-    )
+    val secondmodifier = Modifier.height(100.dp)
+
+    Column(
+        Modifier.fillMaxSize().padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            "Hello Compose",
+            mymodifier.then(secondmodifier),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(Modifier.padding(16.dp))
+
+        CustomImage(image = R.drawable.kistunagi,
+            Modifier
+                .padding(16.dp)
+                .width(300.dp)
+                .clip(shape = RoundedCornerShape(30.dp)))
+    }
 }
 
 @Composable
